383132b7ab868443b6d458ce0d897192f7c214a7
shammu525 <fiberathome71@gmail.com>
2017-06-17 12:11:46 +0600

    second commit

---
+++ src/SeleniumTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class SeleniumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","E:/Selenium files/gecko/geckodriver")
+WebDriver driver = new FirefoxDriver();
+//WebDriver chdriver= new ChromeDriver();
+//WebDriver iedriver= new InternetExplorerDriver();
+
+
+
+	}
+
+}
